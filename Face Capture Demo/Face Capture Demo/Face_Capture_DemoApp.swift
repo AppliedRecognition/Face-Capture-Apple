68c487a06a0de67601b60ba68c2f4d93e7090834
Jakub Dolejs <jakubdolejs@gmail.com>
2024-02-06 19:28:04 +0000

    Added demo app

---
+++ Face Capture Demo/Face Capture Demo/Face_Capture_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_Capture_DemoApp.swift
+//  Face Capture Demo
+//
+//  Created by Jakub Dolejs on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Face_Capture_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
